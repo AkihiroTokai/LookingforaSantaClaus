bc2f5c847c1a26c8d174985f46cffc0146216d82
TokaiAkihiro <contact@techrelation.xyz>
2016-08-31 20:23:34 +0900

    Refactoring:showScore,addScore,string.xml

--- app/src/main/java/xyz/techrelation/lookingforasantaclaus/MainActivity.java
+++ app/src/main/java/xyz/techrelation/lookingforasantaclaus/MainActivity.java
@@ -31,6 +31,7 @@ public class MainActivity extends AppCompatActivity {
     private int timerM;
     private int no_target;
 
+    private static final int NOTFOUND = 404;
 
     private boolean hasStarted = false;
     private boolean first_click = false;
@@ -88,7 +89,7 @@ public class MainActivity extends AppCompatActivity {
                         mainTimer.cancel();
                         game_count += 1;
                         timerM = 2000;
-                        statusView.setText("Level2");
+                        statusView.setText(R.string.lv2_st);
                         timers();
                         place_targetA = rnd.nextInt(15);
                         imageViews[place_targetA].setImageResource(R.drawable.present_box2);
@@ -97,7 +98,7 @@ public class MainActivity extends AppCompatActivity {
                         mainTimer.cancel();
                         game_count += 1;
                         timerM = 1500;
-                        statusView.setText("Level3");
+                        statusView.setText(R.string.lv3_st);
                         timers();
                         place_targetA = rnd.nextInt(15);
                         imageViews[place_targetA].setImageResource(R.drawable.present_box3);
@@ -118,7 +119,7 @@ public class MainActivity extends AppCompatActivity {
                             isBonusStage = true;
                             game_count += 1;
                             timerM = 1300;
-                            statusView.setText("BonusStage");
+                            statusView.setText(R.string.bnstage_st);
                             timers();
                             place_targetA = rnd.nextInt(15);
                             type_targetA = 4;
@@ -167,7 +168,7 @@ public class MainActivity extends AppCompatActivity {
                             }
                         }
                         first_click = true;
-                        statusView.setText("Finish!");
+                        statusView.setText(R.string.finish_st);
                         setup();
                     }
                 }
@@ -199,24 +200,20 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void Start(View view) {
-        score = 0;
+        changeScore(0,NOTFOUND);
         timerM = 3000;
-        scoreView.setText(String.valueOf(score));
         mainTimer = new Timer();
         MainTimerTask = new MainTimerTask();
         mainTimer.schedule(MainTimerTask, 3000, timerM);
-        statusView.setText("Level1");
+        statusView.setText(R.string.lv1_st);
         hasStarted = true;
-
     }
 
     public void Reset(View view) {
-        if (hasStarted == true) {
+        if (hasStarted) {
             mainTimer.cancel();
             MainTimerTask.cancel();
-            score = 0;
-            scoreView.setText(String.valueOf(score));
-            statusView.setText("Resetしました。");
+            changeScore(0,NOTFOUND);
             game_count = 0;
             hasStarted = false;
 
@@ -232,44 +229,34 @@ public class MainActivity extends AppCompatActivity {
     public void find(View view) {
         int click_number = Integer.parseInt((String) view.getTag());
         //レベル1でtarget_numberを押したとき
-        if (first_click == true && hasStarted == true && game_count < 11 && click_number == place_targetA) {
-            score = score + 15;
+        if (first_click && hasStarted && game_count < 11 && click_number == place_targetA) {
+            changeScore(15, click_number);
             countlv1 = countlv1++;
-            scoreView.setText(score + "Point");
             first_click = false;
-            imageViews[place_targetA].setImageResource(R.drawable.plus15);
         }
 
         //レベル1でtarget_number以外を押したとき
-        else if (hasStarted == true && game_count < 11 && click_number != place_targetA) {
-            score = score - 10;
-            scoreView.setText(score + "Point");
-            imageViews[click_number].setImageResource(R.drawable.minus10);
+        else if (hasStarted && game_count < 11 && click_number != place_targetA) {
+            changeScore(-10, click_number);
         }
 
         //レベル2でtarget_numberを押したとき
-        else if (first_click == true && game_count > 10 && game_count < 21 && click_number == place_targetA) {
-            score = score + 20;
+        else if (first_click && game_count > 10 && game_count < 21 && click_number == place_targetA) {
+            changeScore(20, click_number);
             countlv2 = countlv2++;
-            scoreView.setText(score + "Point");
             first_click = false;
-            imageViews[place_targetA].setImageResource(R.drawable.plus20);
         }
 
         //レベル2でtarget_number以外を押したとき
         else if (game_count > 10 && game_count < 21 && click_number != place_targetA) {
-            score = score - 15;
-            scoreView.setText(score + "Point");
-            imageViews[click_number].setImageResource(R.drawable.minus15);
+            changeScore(-15, click_number);
         }
 
         //レベル3でbonus_numberを押さずtarget_numberを押したとき
-        else if (first_click == true && game_count > 21 && click_number == place_targetA && !click_bonus) {
-            score = score + 25;
+        else if (first_click && game_count > 21 && click_number == place_targetA && !click_bonus) {
+            changeScore(25, click_number);
             countlv3 = countlv3++;
-            scoreView.setText(score + "Point");
             first_click = false;
-            imageViews[place_targetA].setImageResource(R.drawable.plus25);
         }
 
         //レベル3とBonusStageでbonus_numberを押したとき
@@ -279,19 +266,15 @@ public class MainActivity extends AppCompatActivity {
 
         //レベル3でbonus_numberを押したあとtarget_numberを押したとき
         else if (game_count > 21 && click_number == place_targetA && click_bonus) {
-            score = score + 40;
+            changeScore(40, click_number);
             if (!isBonusStage) {
                 countlv3 = countlv3++;
             }
-            scoreView.setText(score + "Point");
-            imageViews[place_targetA].setImageResource(R.drawable.plus40);
         }
 
         //レベル3でtarget_numberとbonus_number以外を押したとき
         else if (game_count > 21 && click_number != place_targetA && click_number != bonus_number) {
-            score = score - 20;
-            scoreView.setText(score + "Point");
-            imageViews[place_targetA].setImageResource(R.drawable.minus20);
+            changeScore(-20, click_number);
 
         }
 
@@ -300,155 +283,127 @@ public class MainActivity extends AppCompatActivity {
             if (!click_bonus) {
                 switch (type_targetA) {
                     case 1:
-                        score = score + 15;
-                        scoreView.setText(score + "Point");
+                        changeScore(15,NOTFOUND);
                         imageViews[place_targetA].setImageResource(R.drawable.plus15);
                         break;
                     case 2:
-                        score = score + 20;
-                        scoreView.setText(score + "Point");
+                        changeScore(20,NOTFOUND);
                         imageViews[place_targetA].setImageResource(R.drawable.plus20);
                         break;
                     case 3:
-                        score = score + 25;
-                        scoreView.setText(score + "Point");
+                        changeScore(25,NOTFOUND);
                         imageViews[place_targetA].setImageResource(R.drawable.plus25);
                         break;
                     case 4:
-                        score = score + 30;
-                        scoreView.setText(score + "Point");
+                        changeScore(30,NOTFOUND);
                         imageViews[place_targetA].setImageResource(R.drawable.plus30);
                         break;
                 }
             } else {
-                score = score + 40;
-                scoreView.setText(score + "Point");
+                changeScore(40,NOTFOUND);
                 imageViews[place_targetA].setImageResource(R.drawable.plus40);
                 click_bonus = false;
             }
         }
 
         //BonusStageでtarget_numberBを押したとき
-        else if (isBonusStage == true && click_number == place_targetB) {
+        else if (isBonusStage && click_number == place_targetB) {
             if (!click_bonus) {
                 switch (type_targetB) {
                     case 1:
-                        score = score + 15;
-                        scoreView.setText(score + "Point");
+                        changeScore(15,NOTFOUND);
                         imageViews[place_targetB].setImageResource(R.drawable.plus15);
                         break;
                     case 2:
-                        score = score + 20;
-                        scoreView.setText(score + "Point");
+                        changeScore(20,NOTFOUND);
                         imageViews[place_targetB].setImageResource(R.drawable.plus20);
                         break;
                     case 3:
-                        score = score + 25;
-                        scoreView.setText(score + "Point");
+                        changeScore(25,NOTFOUND);
                         imageViews[place_targetB].setImageResource(R.drawable.plus25);
                         break;
                     case 4:
-                        score = score + 30;
-                        scoreView.setText(score + "Point");
+                        changeScore(30,NOTFOUND);
                         imageViews[place_targetB].setImageResource(R.drawable.plus30);
                         break;
 
                 }
             } else {
-                score = score + 40;
-                scoreView.setText(score + "Point");
+                changeScore(40,NOTFOUND);
                 imageViews[place_targetB].setImageResource(R.drawable.plus40);
                 click_bonus = false;
             }
         }
         //BonusStageでtarget_numberCを押したとき
-        else if (isBonusStage == true && click_number == place_targetC) {
+        else if (isBonusStage && click_number == place_targetC) {
             if (!click_bonus) {
                 switch (type_targetC) {
                     case 1:
-                        score = score + 15;
-                        scoreView.setText(score + "Point");
+                        changeScore(15,NOTFOUND);
                         imageViews[place_targetC].setImageResource(R.drawable.plus15);
                         break;
                     case 2:
-                        score = score + 20;
-                        scoreView.setText(score + "Point");
+                        changeScore(20,NOTFOUND);
                         imageViews[place_targetC].setImageResource(R.drawable.plus20);
                         break;
                     case 3:
-                        score = score + 25;
-                        scoreView.setText(score + "Point");
+                        changeScore(25,NOTFOUND);
                         imageViews[place_targetC].setImageResource(R.drawable.plus25);
                         break;
                     case 4:
-                        score = score + 35;
-                        scoreView.setText(score + "Point");
+                        changeScore(30,NOTFOUND);
                         imageViews[place_targetC].setImageResource(R.drawable.plus30);
                         break;
                 }
             } else {
-                score = score + 40;
-                scoreView.setText(score + "Point");
+                changeScore(40,NOTFOUND);
                 imageViews[place_targetC].setImageResource(R.drawable.plus40);
                 click_bonus = false;
             }
 
         }
         //BonusStageでtarget_numberDを押したとき
-        else if (isBonusStage == true && click_number == place_targetB) {
+        else if (isBonusStage && click_number == place_targetD) {
             if (!click_bonus) {
                 switch (type_targetD) {
                     case 1:
-                        score = score + 15;
-                        scoreView.setText(score + "Point");
+                        changeScore(15,NOTFOUND);
                         imageViews[place_targetD].setImageResource(R.drawable.plus15);
                         break;
                     case 2:
-                        score = score + 20;
-                        scoreView.setText(score + "Point");
+                        changeScore(20,NOTFOUND);
                         imageViews[place_targetD].setImageResource(R.drawable.plus20);
                         break;
                     case 3:
-                        score = score + 25;
-                        scoreView.setText(score + "Point");
+                        changeScore(25,NOTFOUND);
                         imageViews[place_targetD].setImageResource(R.drawable.plus25);
                         break;
                     case 4:
-                        score = score + 30;
-                        scoreView.setText(score + "Point");
+                        changeScore(30,NOTFOUND);
                         imageViews[place_targetD].setImageResource(R.drawable.plus30);
                         break;
                 }
             } else {
-                score = score + 40;
-                scoreView.setText(score + "Point");
+                changeScore(40,NOTFOUND);
                 imageViews[place_targetD].setImageResource(R.drawable.plus40);
                 click_bonus = false;
             }
         }
 
         //BonusStageでtarget以外を押したとき
-        else if (isBonusStage  && (click_number != place_targetA || click_number != place_targetB || click_number != place_targetC)) {
+        else if (isBonusStage && (click_number != place_targetA || click_number != place_targetB || click_number != place_targetC)) {
             switch (no_target) {
                 case 1:
-                    score = score - 30;
-                    scoreView.setText(score + "Point");
-                    imageViews[click_number].setImageResource(R.drawable.minus30);
+                    changeScore(-30, click_number);
                     break;
                 case 2:
-                    score = score - 20;
-                    scoreView.setText(score + "Point");
-                    imageViews[click_number].setImageResource(R.drawable.minus20);
+                    changeScore(-20, click_number);
                     break;
                 case 3:
-                    score = score - 15;
-                    scoreView.setText(score + "Point");
-                    imageViews[click_number].setImageResource(R.drawable.minus15);
+                    changeScore(-15, click_number);
                     break;
                 case 4:
-                    score = score - 10;
-                    scoreView.setText(score + "Point");
-                    imageViews[click_number].setImageResource(R.drawable.minus10);
+                    changeScore(-10, click_number);
                     break;
 
             }
@@ -457,6 +412,36 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
+    public void changeScore(int getScore, int click_number) {
+        score += getScore;
+        scoreView.setText(score + R.string.unit_sc);
+        if (click_number!= NOTFOUND) {
+            switch (getScore) {
+                case -30:
+                    imageViews[click_number].setImageResource(R.drawable.minus30);
+                    break;
+                case -20:
+                    imageViews[click_number].setImageResource(R.drawable.minus20);
+                    break;
+                case -10:
+                    imageViews[click_number].setImageResource(R.drawable.minus10);
+                    break;
+                case 15:
+                    imageViews[click_number].setImageResource(R.drawable.plus15);
+                    break;
+                case 20:
+                    imageViews[click_number].setImageResource(R.drawable.plus20);
+                    break;
+                case 30:
+                    imageViews[click_number].setImageResource(R.drawable.plus30);
+                    break;
+                case 40:
+                    imageViews[click_number].setImageResource(R.drawable.plus40);
+                    break;
+
+            }
+        }
+    }
 
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
